193b3452a77985c4bcdd3353c790769d7e33b551
Subash Prasad <86200480+Subash62036@users.noreply.github.com>
2022-01-20 12:20:22 +0530

    Update Arrayadd.java

--- Arrayadd.java
+++ Arrayadd.java
@@ -1,53 +1,41 @@
-/*WAP to add all the elements of an array, WAP
-to print second largesr element of an array.*/
-import java.util.*;
-public class Arrayadd
- {
-   public static void main(String [] args)
-    {
-	   int arr[] =new int [5],sum=0,large=0,seclarge=0;
-	   System.out.println("Enter any five nos into array");
-        Scanner ref=new Scanner(System.in);
-         for(int i=0;i<5;i++)
-           {
-             arr[i]=ref.nextInt();
+import java.util.Scanner;
+
+public class ArrayAdd {
+    public void sum(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            sum = sum + arr[i];
+        }
+        System.out.println("sum of array elements =" + sum);
+    }
+
+    public void SeondLarge(int[] arr) {
+        int temp, i;
+        for (i = 0; i < 5; i++) {
+            for (int j = 0; j < 4 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
             }
-       // for sum
-	   
-	   // first i am going to find large no 
-	     large=arr[0];
-         for(int i=0;i<5;i++)
-            {
-              sum=sum+arr[i];	
-			  if(large<arr[i])
-			  {
-				  large=arr[i];
-			  }
-			}
-			
-		   /*first  of all i am initilizing seclarge variable with any lement of given array except large value 
-		   this block only for that*/
-			
-			for(int i=0;i<5;i++)
-			{
-				if(arr[i]!=large)
-				{
-				seclarge=arr[i];
-				  break;
-				}
-				
-			}
-			// Now i am seaching second large no with this block;
-			for(int i=0;i<5;i++)
-			{
-				 if(seclarge<arr[i] && arr[i]!=large)
-				 {
-					 seclarge=arr[i];
-				 }
-			}
-			
-				
-       System.out.println("Sum of array lements =" + sum +"\n Second large value of array="+ seclarge);
-	   
-	}
- }	
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        int [] arr =new int [5];
+        int i;
+        System.out.println("Enter any five number");
+        Scanner sc = new Scanner(System.in);
+        for(i = 0 ; 5 > i; i++) {
+            arr[i] = sc.nextInt();
+        }
+        ArrayAdd ref = new ArrayAdd();
+        ref.sum(arr);
+        ref.SeondLarge( arr);
+        System.out.println(" Second Largest no of array =" + arr[3]);
+
+    }
+}
